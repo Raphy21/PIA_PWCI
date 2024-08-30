b2a1d4b66373e80a9bb1df90a9fc52b5140963f3
Bryan Pardo <bpardo0511@hotmail.com>
2024-08-29 20:57:16 -0600

    Cambios a los CSS

--- js/footer.js
+++ js/footer.js
@@ -1,15 +1,15 @@
-function posicionFooter() {
-    const footer = document.getElementById('idFin');
-    const bodyHeight = document.body.scrollHeight;
-    const windowHeight = window.innerHeight;
-    if (bodyHeight > windowHeight) {
-        footer.style.position = 'relative';
-        footer.style.bottom = 'auto';
-    } else {
-        footer.style.position = 'absolute';
-        footer.style.bottom = '0';
-    }
-}
+//function posicionFooter() {
+   // const footer = document.getElementById('idFin');
+    //const bodyHeight = document.body.scrollHeight;
+    //const windowHeight = window.innerHeight;
+    //if (bodyHeight > windowHeight) {
+      //  footer.style.position = 'relative';
+        //footer.style.bottom = 'auto';
+    //} else {
+        //footer.style.position = 'absolute';
+        //footer.style.bottom = '0';
+    //}
+//}//
 
-window.addEventListener('resize', posicionFooter);
-window.addEventListener('load', posicionFooter);
+//window.addEventListener('resize', posicionFooter);
+//window.addEventListener('load', posicionFooter);/
